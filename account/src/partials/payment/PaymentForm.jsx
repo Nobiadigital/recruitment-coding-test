1bb0e728f028c44934939f5bb851456378902d45
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-06-29 10:42:32 +0200

    Remove adyen from app

--- account/src/partials/payment/PaymentForm.jsx
+++ account/src/partials/payment/PaymentForm.jsx
@@ -1,7 +1,6 @@
 import Input from "../../components/form/Input";
 import Button from "../../components/button/Button";
 import { useEffect, useState, useRef } from "react";
-import adyenCheckout from "@adyen/adyen-web";
 
 const paymentResults = {
   NONE: 'none',
@@ -22,74 +21,6 @@ const PaymentForm = () => {
     setStartPaying(false);
     setPaymentCompleted(false);
   };
-
-  useEffect(() => {
-    if(adyenWrapper.current){
-      (async () => {
-        const url = `${window.PUBLIC_APP_SETTING_BACKEND_API}payment/session?returnUrl=${encodeURIComponent(window.location.href + '?completed=true')}&amount=${amountToPay}`;
-        const options = {
-          mode: "cors",
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json;charset=UTF-8",
-            Authorization: `Bearer ${(window.__OMNI__ && window.__OMNI__.access_token) || prompt('bearer token? get one from tokens api!')}`,
-          }
-        };
-
-        const res = await fetch(url, options);
-        let configuration = await res.json();
-        setPaymentSession(configuration);
-        setPaymentResult(paymentResults.NONE);
-
-        configuration = {
-          ...configuration,
-          locale: "en_US",
-          showPayButton: true,
-          paymentMethodsConfiguration: {
-            ideal: {
-              showImage: true
-            },
-            card: {
-              hasHolderName: true,
-              holderNameRequired: true,
-              name: "Credit or debit card",
-              amount: {
-                value: configuration.amount,
-                currency: "GBP"
-              }
-            },
-          },
-          onPaymentCompleted: (result, component) => {
-            setPaymentSession(null);
-            setPaymentCompleted(true);
-            setPaymentResult(!!result && result.resultCode === 'Authorised'
-              ? paymentResults.SUCCESSFUL
-              : paymentResults.FAIL);
-
-              component.unmount();
-          },
-          onError: (error, component) => {
-            setPaymentCompleted(true);
-            setPaymentResult(paymentResults.FAIL);
-            console.error(error.name, error.message, error.stack, component);
-            component.unmount();
-          }
-        };
-
-        const checkout = await new adyenCheckout(configuration);
-
-        adyenWrapper.current.innerHTML = '';
-        checkout.create('card').mount(adyenWrapper.current);
-      })();
-    }
-
-    return () => {
-      if(adyenWrapper.current){
-        // shutdown adyen if needed
-      }
-    };
-  }, [startPaying, amountToPay]);
 
   if(!startPaying && !paymentCompleted){
     return (
