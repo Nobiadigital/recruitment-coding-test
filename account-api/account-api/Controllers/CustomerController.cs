ba8a49910c479fe9868716764c483645a96c28bc
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-07-01 15:48:27 +0200

    Add Dependency Injection problem

--- account-api/account-api/Controllers/CustomerController.cs
+++ account-api/account-api/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MagnetTradeAccountApi.Injected;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagnetTradeAccountApi.Controllers
@@ -7,6 +8,11 @@ namespace MagnetTradeAccountApi.Controllers
     [Route("customer")]
     public class CustomerController : Controller
     {
+        public CustomerController(IThisIsInjected thisIsInjected)
+        {
+            thisIsInjected.Hello(); // Don't remove this! Fix the Dependency Injection instead!
+        }
+
         [HttpGet("information-fake")]
         public MagnetTradeCustomer GetFake()
         {
